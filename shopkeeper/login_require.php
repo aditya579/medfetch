fbb6d827a98cd01ce6fa778a6a4aad67dd2b3ef2
aditya579 <pandey.aditya579@gmail.com>
2018-12-04 20:32:15 +0530

    add new file

--- shopkeeper/login_require.php
+++ shopkeeper/login_require.php
@@ -1,6 +1,7 @@
 <?php
 if(isset($_POST['username']) && isset($_POST['password'])){
-		define("DB_SERVER", "localhost");
+	
+	define("DB_SERVER", "localhost");
     define("DB_USERNAME", "root");
     define("DB_PASSWORD", "1234567");
     define("DB_DATABASE", "medicine");
@@ -9,14 +10,17 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 
 
 		$user_name = 'root';
-		$password = '1234567';
-		$db_name = 'medicine';
+		$password  = '1234567';
+		$db_name   = 'medicine';
+		
 		$con= mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD);
+		
 		if(!$con){
 			echo'<script language = javascript>';
 			echo'alert("server is down")';
 			echo'</script>';
 		}
+		
 		if(! get_magic_quotes_gpc())
 		{
 			$un = addslashes($_POST['username']);
@@ -27,24 +31,25 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 			$un = $_POST['username'];
 			$pass = $_POST['password'];
 		}
+		
 		mysqli_select_db($con,$db_name);
+		
 		$sql = "SELECT id FROM shop_detail WHERE (shop_name = '$un') AND (password = '$pass')";
+		
 		$result = mysqli_query($con,$sql);
+		
 		$row=mysqli_fetch_row($result);
+		
 		if ($row[0] != 0){
 			echo'<script language= "javascript">';
-			echo'
-        window.location.href = "shop_page.php";';
+			echo'window.location.href = "shop_page.php";';
 			echo'</script>';
 			setcookie("name", $_POST['username'], time()+36000, "/","", 0);
-			
-
 		}
 		else{
 			echo'<script language="javascript">';
 			echo'alert("EMAIL OR PASSWORD IS WRONG");';
 			echo'</script>';
 		}
-
 }
 ?>
